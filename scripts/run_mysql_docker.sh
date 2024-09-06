7be2650b93a08f6305c5d6d8b9011365a57d9991
ramiro-l <ramideporte@gmial.com>
2024-09-06 17:23:18 -0300

    feat: add config lab.

--- scripts/run_mysql_docker.sh
+++ scripts/run_mysql_docker.sh
@@ -7,3 +7,8 @@ docker run -p 3306:3306 --name mysql-labs -e MYSQL_ROOT_PASSWORD=admin -d mysql:
 IP=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mysql-labs)
 
 mysql --host $IP -u root -p
+
+# en sql
+CREATE DATABASE world
+
+mysql --host $IP -u root -p < ./script.sql
