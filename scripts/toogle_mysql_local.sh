30bde81d10cd212df5929936c4c5d99bc8eb74ba
ramiro-l <ramideporte@gmail.com>
2024-09-13 19:39:37 -0300

    feat: nuevo comandos utiles.

--- scripts/toogle_mysql_local.sh
+++ scripts/toogle_mysql_local.sh
@@ -1,5 +1,6 @@
 ## Intercambiar entre activado y no activo el mysql local
-sudo systemctl start mysql # Activar
-sudo systemctl stop mysql  # Desactivar
+sudo systemctl start mysql   # Activar
+sudo systemctl stop mysql    # Desactivar
+sudo systemctl restart mysql # Reiniciar mysql local
 
 mysql -h localhost -u root -p # Conectar a mysql local
