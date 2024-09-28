fde64637eb1fcfeb4f26aac577ada215af53d13e
ramiro-l <ramideporte@gmail.com>
2024-09-28 13:28:33 -0300

    feat: se termino hasta el ej 7  del p6.

--- Practico6/CONSULTAS.sql
+++ Practico6/CONSULTAS.sql
@@ -99,12 +99,75 @@ CALL update_credit(103, 10.00);
 /*
  5. Cree una vista "Premium Customers" que devuelva el top 10 de clientes que más dinero han gastado en la plataforma. La vista deberá devolver el nombre del cliente, la ciudad y el total gastado por ese cliente en la plataforma.
  */
+CREATE VIEW `PremiumCustomers` AS(
+    SELECT
+        c.customerName,
+        c.city,
+        sum(p.amount) AS total_amount
+    FROM
+        customers AS c
+        JOIN payments AS p ON c.customerNumber = p.customerNumber
+    GROUP BY
+        c.customerNumber
+    ORDER BY
+        total_amount DESC
+    LIMIT
+        10
+);
+
+-- TEST:
+SELECT
+    *
+FROM
+    `PremiumCustomers`;
+
 /*
  6. Cree una función "employee of the month" que tome un mes y un año y devuelve el empleado (nombre y apellido) cuyos clientes hayan efectuado la mayor cantidad de órdenes en ese mes.
  */
+CREATE FUNCTION employee_of_the_month(mes INT, anio INT) RETURNS VARCHAR(200) READS SQL DATA BEGIN DECLARE employee_name VARCHAR(200);
+
+SELECT
+    CONCAT(e.firstName, ' ', e.lastName) INTO employee_name
+FROM
+    employees AS e
+    JOIN customers AS c ON c.salesRepEmployeeNumber = e.employeeNumber
+    JOIN orders AS o ON o.customerNumber = c.customerNumber
+WHERE
+    DATE_FORMAT(o.requiredDate, '%Y') = anio
+    AND DATE_FORMAT(o.requiredDate, '%m') = mes
+GROUP BY
+    e.employeeNumber
+ORDER BY
+    COUNT(*) DESC
+LIMIT
+    1;
+
+if employee_name is null then
+SET
+    employee_name = 'No hay datos';
+
+end if;
+
+RETURN employee_name;
+
+END;
+
+-- DROP FUNCTION employee_of_the_month;
+-- TEST:
+SELECT
+    employee_of_the_month(1, 2003) AS mejor_empleado;
+
 /*
  7. Crear una nueva tabla "Product Refillment". Deberá tener una relación varios a uno con "products" y los campos: `refillmentID`, `productCode`, `orderDate`, `quantity`.
  */
+CREATE TABLE `product_refillment` (
+    `refillmentID` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `productCode` varchar(15) NOT NULL,
+    `orderDate` date NOT NULL,
+    `quantity` INT(10) NOT NULL,
+    FOREIGN KEY (`productCode`) REFERENCES products (`productCode`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
 /*
  8. Definir un trigger "Restock Product" que esté pendiente de los cambios efectuados en `orderdetails` y cada vez que se agregue una nueva orden revise la cantidad de productos pedidos (`quantityOrdered`) y compare con la cantidad en stock (`quantityInStock`) y si es menor a 10 genere un pedido en la tabla "Product Refillment" por 10 nuevos productos.
  */
@@ -113,7 +176,7 @@ CALL update_credit(103, 10.00);
  */
 -- Consultas Adicionales
 /*
- 1. Encontrar, para cada cliente de aquellas ciudades que comienzan por 'N', la menor y la mayor diferencia en días entre las fechas de sus pagos. No mostrar el id del cliente, sino su nombre y el de su contacto.
+ 1. Encontrar, para cada cliente de aquellas ciudades que comienzan por ' N ', la menor y la mayor diferencia en días entre las fechas de sus pagos. No mostrar el id del cliente, sino su nombre y el de su contacto.
  */
 /*
  2. Encontrar el nombre y la cantidad vendida total de los 10 productos más vendidos que, a su vez, representen al menos el 4% del total de productos, contando unidad por unidad, de todas las órdenes donde intervienen. No utilizar LIMIT.
