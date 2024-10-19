60deb923fe611e3a00e41a3f4e8f2e1d1115c168
ramiro-l <ramideporte@gmail.com>
2024-10-19 09:00:13 -0300

    feat: se organizaron los archivos para  las distintas partes del practico1.

---
+++ MONGODB/Practico1/CONSULTAS-P2.mongodb.js
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------------------
+//  PARTE 2
+// ---------------------------------------------------------------------------------------
+
+use("mflix");
+
+// ---------------------------------------------------------------------------------------
+// 10. Listar el id del restaurante (restaurant_id) y las calificaciones de los
+//     restaurantes donde al menos una de sus calificaciones haya sido realizada
+//     entre 2014 y 2015 inclusive, y que tenga una puntuación(score)
+//     mayor a 70 y menor o igual a 90.
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------
+// 11. Agregar dos nuevas calificaciones al restaurante cuyo id es "50018608".
+//     A continuación se especifican las calificaciones a agregar en una sola consulta.
+//
+//     {
+//     	"date" : ISODate("2019-10-10T00:00:00Z"),
+//     	"grade" : "A",
+//     	"score" : 18
+//     }
+//
+//     {
+//     	"date" : ISODate("2020-02-25T00:00:00Z"),
+//     	"grade" : "A",
+//     	"score" : 21
+//     }
+//
+// ---------------------------------------------------------------------------------------
