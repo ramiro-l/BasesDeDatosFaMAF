e85a439462c20f1d1266142b9e55b52f5bc952ae
ramiro-l <ramideporte@gmail.com>
2024-10-19 09:35:26 -0300

    feat: se termino la parte 2 del p1.

--- MONGODB/Practico1/CONSULTAS-P2.mongodb.js
+++ MONGODB/Practico1/CONSULTAS-P2.mongodb.js
@@ -2,7 +2,8 @@
 //  PARTE 2
 // ---------------------------------------------------------------------------------------
 
-use("mflix");
+use("restaurantdb");
+db.restaurants.findOne();
 
 // ---------------------------------------------------------------------------------------
 // 10. Listar el id del restaurante (restaurant_id) y las calificaciones de los
@@ -11,20 +12,72 @@ use("mflix");
 //     mayor a 70 y menor o igual a 90.
 // ---------------------------------------------------------------------------------------
 
+//db.restaurants.find();
+
+db.restaurants.find(
+  {
+    grades: {
+      $elemMatch: {
+        date: {
+          $gte: ISODate("2014-01-01"),
+          $lte: ISODate("2015-12-31"),
+        },
+        score: {
+          $gt: 70,
+          $lte: 90,
+        },
+      },
+    },
+  },
+  {
+    restaurant_id: 1,
+    grades: 1,
+    _id: 0,
+  }
+);
+
+// OBS: ( no hace falta poner un AND dentro del elemMatch, esta implicito )
+
 // ---------------------------------------------------------------------------------------
 // 11. Agregar dos nuevas calificaciones al restaurante cuyo id es "50018608".
 //     A continuación se especifican las calificaciones a agregar en una sola consulta.
 //
-//     {
-//     	"date" : ISODate("2019-10-10T00:00:00Z"),
-//     	"grade" : "A",
-//     	"score" : 18
-//     }
-//
-//     {
-//     	"date" : ISODate("2020-02-25T00:00:00Z"),
-//     	"grade" : "A",
-//     	"score" : 21
-//     }
+// {
+// 	"date" : ISODate("2019-10-10T00:00:00Z"),
+// 	"grade" : "A",
+// 	"score" : 18
+// }
+
+// {
+// 	"date" : ISODate("2020-02-25T00:00:00Z"),
+// 	"grade" : "A",
+// 	"score" : 21
+// }
 //
 // ---------------------------------------------------------------------------------------
+
+// db.restaurants.find({ restaurant_id: "50018608" });
+
+db.restaurants.updateOne(
+  {
+    restaurant_id: "50018608",
+  },
+  {
+    $push: {
+      grades: {
+        $each: [
+          {
+            date: ISODate("2019-10-10T00:00:00Z"),
+            grade: "A",
+            score: 18,
+          },
+          {
+            date: ISODate("2020-02-25T00:00:00Z"),
+            grade: "A",
+            score: 21,
+          },
+        ],
+      },
+    },
+  }
+);
