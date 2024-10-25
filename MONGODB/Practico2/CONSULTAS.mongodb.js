13517989769174e2748c752af91a8223a4ea3845
ramiro-l <ramideporte@gmail.com>
2024-10-25 15:56:53 -0300

    fix: formateando las preguntas.

--- MONGODB/Practico2/CONSULTAS.mongodb.js
+++ MONGODB/Practico2/CONSULTAS.mongodb.js
@@ -34,7 +34,8 @@ use("mflix");
 // ---------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------
-// 7. Ratings de IMDB promedio, mínimo y máximo por año de las películas estrenadas en los años 80 (desde 1980 hasta 1989), ordenados de mayor a menor por promedio del año.
+// 7. Ratings de IMDB promedio, mínimo y máximo por año de las películas estrenadas en 
+//    los años 80 (desde 1980 hasta 1989), ordenados de mayor a menor por promedio del año.
 // ---------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------
