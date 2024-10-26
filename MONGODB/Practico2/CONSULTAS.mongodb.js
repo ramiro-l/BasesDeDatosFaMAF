e1362d55f3aff192368534423e17da261b380048
ramiro-l <ramideporte@gmail.com>
2024-10-26 12:15:29 -0300

    feat: se avanzo en el p2 de mongo.

--- MONGODB/Practico2/CONSULTAS.mongodb.js
+++ MONGODB/Practico2/CONSULTAS.mongodb.js
@@ -11,13 +11,12 @@ use("mflix");
 // db.theaters.findOne();
 
 db.theaters.aggregate([
-    {
-        $group: {
-            _id: "$location.address.state",
-            count: { $sum: 1 }
-
-        }
+  {
+    $group: {
+      _id: "$location.address.state",
+      count: { $sum: 1 },
     },
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
@@ -25,17 +24,17 @@ db.theaters.aggregate([
 // ---------------------------------------------------------------------------------------
 
 db.theaters.aggregate([
-    {
-        $group: {
-            _id: "$location.address.state",
-            count: { $sum: 1 }
-        }
+  {
+    $group: {
+      _id: "$location.address.state",
+      count: { $sum: 1 },
     },
-    {
-        $match: {
-            "count": { $gte: 2 }
-        }
-    }
+  },
+  {
+    $match: {
+      count: { $gte: 2 },
+    },
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
@@ -45,19 +44,21 @@ db.theaters.aggregate([
 
 // db.movies.findOne();
 
-db.movies.find({
-    directors: { $elemMatch: { $regex: /Louis Lumière/i } }
-}).count();
+db.movies
+  .find({
+    directors: { $elemMatch: { $regex: /Louis Lumière/i } },
+  })
+  .count();
 
 db.movies.aggregate([
-    {
-        $match: {
-            directors: { $elemMatch: { $regex: /Louis Lumière/i } }
-        }
+  {
+    $match: {
+      directors: { $elemMatch: { $regex: /Louis Lumière/i } },
     },
-    {
-        $count: "total_pelis_de_louis"
-    }
+  },
+  {
+    $count: "total_pelis_de_louis",
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
@@ -67,29 +68,31 @@ db.movies.aggregate([
 
 db.movies.findOne();
 
-db.movies.find({
+db.movies
+  .find({
     year: {
-        $gte: 1950,
-        $lt: 1960
-    }
-}).count();
+      $gte: 1950,
+      $lt: 1960,
+    },
+  })
+  .count();
 
 db.movies.aggregate([
-    {
-        $match: {
-            year: {
-                $gte: 1950,
-                $lt: 1960
-            }
-        }
+  {
+    $match: {
+      year: {
+        $gte: 1950,
+        $lt: 1960,
+      },
     },
-    {
-        $count: "cant_pelis_en_50s"
-    }
+  },
+  {
+    $count: "cant_pelis_en_50s",
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
-// 5. Listar los 10 géneros con mayor cantidad de películas (tener en cuenta que las 
+// 5. Listar los 10 géneros con mayor cantidad de películas (tener en cuenta que las
 //    películas pueden tener más de un género). Devolver el género y la cantidad de
 //    películas. Hint: unwind puede ser de utilidad
 // ---------------------------------------------------------------------------------------
@@ -97,81 +100,81 @@ db.movies.aggregate([
 // db.movies.findOne();
 
 db.movies.aggregate([
-    {
-        $unwind: "$genres"
+  {
+    $unwind: "$genres",
+  },
+  {
+    $group: {
+      _id: "$genres",
+      count: { $sum: 1 },
     },
-    {
-        $group: {
-            _id: "$genres",
-            count: { $sum: 1 }
-        }
-    },
-    {
-        $sort: { "count": -1 }
-    },
-    {
-        $limit: 10
-    }
+  },
+  {
+    $sort: { count: -1 },
+  },
+  {
+    $limit: 10,
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
-// 6. Top 10 de usuarios con mayor cantidad de comentarios, mostrando Nombre, Email y 
+// 6. Top 10 de usuarios con mayor cantidad de comentarios, mostrando Nombre, Email y
 //    Cantidad de Comentarios.
 // ---------------------------------------------------------------------------------------
 
 // db.comments.findOne();
 
 db.comments.aggregate([
-    {
-        $group: {
-            _id: { name: "$name", email: "$email" },
-            count: { $sum: 1 }
-        }
+  {
+    $group: {
+      _id: { name: "$name", email: "$email" },
+      count: { $sum: 1 },
     },
-    {
-        $sort: { "count": -1 }
-    },
-    {
-        $limit: 10
-    }
+  },
+  {
+    $sort: { count: -1 },
+  },
+  {
+    $limit: 10,
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
-// 7. Ratings de IMDB promedio, mínimo y máximo por año de las películas estrenadas en 
+// 7. Ratings de IMDB promedio, mínimo y máximo por año de las películas estrenadas en
 //    los años 80 (desde 1980 hasta 1989), ordenados de mayor a menor por promedio del año.
 // ---------------------------------------------------------------------------------------
 
 // db.movies.findOne()
 
 db.movies.aggregate([
-    {
-        $match: {
-            year: {
-                $gte: 1980,
-                $lt: 1990
-            },
-            "imdb.rating": { $type: "number" }
-        }
+  {
+    $match: {
+      year: {
+        $gte: 1980,
+        $lt: 1990,
+      },
+      "imdb.rating": { $type: "number" },
     },
-    {
-        $group: {
-            _id: "$year",
-            promedio: {
-                $avg: "$imdb.rating"
-            },
-            minimo: {
-                $min: "$imdb.rating"
-            },
-            maximo: {
-                $max: "$imdb.rating"
-            }
-        }
+  },
+  {
+    $group: {
+      _id: "$year",
+      promedio: {
+        $avg: "$imdb.rating",
+      },
+      minimo: {
+        $min: "$imdb.rating",
+      },
+      maximo: {
+        $max: "$imdb.rating",
+      },
     },
-    {
-        $sort: {
-            promedio: -1
-        }
-    }
+  },
+  {
+    $sort: {
+      promedio: -1,
+    },
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
@@ -181,78 +184,328 @@ db.movies.aggregate([
 // db.comments.findOne();
 
 db.comments.aggregate([
-    {
-        $group: {
-            _id: "$movie_id",
-            cant_comments: { $sum: 1 },
-        }
+  {
+    $group: {
+      _id: "$movie_id",
+      cant_comments: { $sum: 1 },
     },
-    {
-        $lookup: {
-            from: "movies",
-            localField: "_id",
-            foreignField: "_id",
-            as: "movie"
-        }
+  },
+  {
+    $lookup: {
+      from: "movies",
+      localField: "_id",
+      foreignField: "_id",
+      as: "movie",
     },
-    {
-        $unwind: "$movie"
+  },
+  {
+    $unwind: "$movie",
+  },
+  {
+    $project: {
+      title: "$movie.title",
+      year: "$movie.year",
+      cant_comments: 1,
     },
-    {
-        $project: {
-            "title": "$movie.title",
-            "year": "$movie.year",
-            cant_comments: 1
-        }
+  },
+  {
+    $sort: {
+      cant_comments: -1,
     },
-    {
-        $sort: {
-            cant_comments: -1
-        }
-    },
-    {
-        $limit: 10
-    }
+  },
+  {
+    $limit: 10,
+  },
 ]);
 
 // ---------------------------------------------------------------------------------------
-// 9. Crear una vista con los 5 géneros con mayor cantidad de comentarios, junto con 
+// 9. Crear una vista con los 5 géneros con mayor cantidad de comentarios, junto con
 //    la cantidad de comentarios.
 // ---------------------------------------------------------------------------------------
+db.createView("top5_genres_comments", "comments", [
+  {
+    $group: {
+      _id: "$movie_id",
+      cant_comments: { $sum: 1 },
+    },
+  },
+  {
+    $lookup: {
+      from: "movies",
+      localField: "_id",
+      foreignField: "_id",
+      as: "movie",
+    },
+  },
+  {
+    $unwind: "$movie",
+  },
+  {
+    $project: {
+      genres: "$movie.genres",
+      cant_comments: 1,
+    },
+  },
+  {
+    $unwind: "$genres",
+  },
+  {
+    $group: {
+      _id: "$genres",
+      cant_comments: { $sum: "$cant_comments" },
+    },
+  },
+  {
+    $sort: {
+      cant_comments: -1,
+    },
+  },
+  {
+    $limit: 5,
+  },
+]);
+
+db.top5_genres_comments.find();
 
 // ---------------------------------------------------------------------------------------
-// 10. Listar los actores (cast) que trabajaron en 2 o más películas dirigidas por 
-// "Jules Bass". Devolver el nombre de estos actores junto con la lista de películas 
-// (solo título y año) dirigidas por “Jules Bass” en las que trabajaron. 
-//      a. Hint1: addToSet
-//      b. Hint2: {'name.2': {$exists: true}} permite filtrar arrays con al menos 2 
-//         elementos, entender por qué.
-//      c. Hint3: Puede que tu solución no use Hint1 ni Hint2 e igualmente sea correcta
+// 10. Listar los actores (cast) que trabajaron en 2 o más películas dirigidas por
+//     "Jules Bass". Devolver el nombre de estos actores junto con la lista de películas
+//     (solo título y año) dirigidas por “Jules Bass” en las que trabajaron.
+//          a. Hint1: addToSet
+//          b. Hint2: {'name.2': {$exists: true}} permite filtrar arrays con al menos 2
+//             elementos, entender por qué.
+//          c. Hint3: Puede que tu solución no use Hint1 ni Hint2 e igualmente sea correcta
 // ---------------------------------------------------------------------------------------
+
+// db.movies.findOne();
+
+db.movies.aggregate([
+  {
+    $match: {
+      directors: { $elemMatch: { $regex: /Jules Bass/i } },
+    },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      movies: {
+        $addToSet: {
+          // NOTE: notar que si no agregamos el id y hay 2 peliculas con el
+          // mismo nombre, pero distintos directores no lo vamos a detectar porque
+          // addToSet es un conjunto => si matchea el titulo y el año no lo agrega.
+          _id: "$_id",
+
+          title: "$title",
+          year: "$year",
+        },
+      },
+    },
+  },
+  {
+    $match: {
+      // NOTE: Para que exista el elemento 1 debe
+      // exitstir el elemento 0 => movies.length >= 2
+      "movies.1": { $exists: true },
+    },
+  },
+  {
+    $project: {
+      actor_name: "$_id",
+      movies: 1,
+      _id: 0,
+    },
+  },
+]);
 
 // ---------------------------------------------------------------------------------------
 // 11. Listar los usuarios que realizaron comentarios durante el mismo mes de lanzamiento
 //     de la película comentada, mostrando Nombre, Email, fecha del comentario,
-//     título de la película, fecha de lanzamiento. HINT: usar $lookup con 
-//     multiple condiciones 
+//     título de la película, fecha de lanzamiento.
+//     HINT: usar $lookup con multiple condiciones
+// ---------------------------------------------------------------------------------------
+
+// db.users.findOne();
+// db.movies.find(); // "released"
+// db.comments.find(); // "date"
+
+// FIXME: revisar, porque no use multiples $lookup
+db.comments.aggregate([
+  {
+    $lookup: {
+      from: "movies",
+      localField: "movie_id",
+      foreignField: "_id",
+      as: "movie",
+    },
+  },
+  {
+    $unwind: "$movie",
+  },
+  {
+    $match: {
+      $expr: {
+        $and: [
+          { $eq: [{ $month: "$date" }, { $month: "$movie.released" }] },
+          { $eq: [{ $year: "$date" }, { $year: "$movie.released" }] },
+        ],
+      },
+    },
+  },
+  {
+    $project: {
+      user_name: "$name",
+      user_email: "$email",
+      comment_date: "$date",
+      movie_released_date: "$movie.released",
+      movie_title: "$movie.title",
+      _id: 0,
+    },
+  },
+]);
+// ---------------------------------------------------------------------------------------
+
+// Cambiar a esta base de datos para resolver los siguientes ejercicios.
+use("restaurantdb");
+
 // ---------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------
 // 12. Listar el id y nombre de los restaurantes junto con su puntuación máxima, mínima y la
-// suma total. Se puede asumir que el restaurant_id es único.
-//      a. Resolver con $group y accumulators.
-//      b. Resolver con expresiones sobre arreglos (por ejemplo, $sum) pero sin $group.
-//      c. Resolver como en el punto b) pero usar $reduce para calcular la puntuación total.
-//      d. Resolver con find.
+//     suma total. Se puede asumir que el restaurant_id es único.
+//        a. Resolver con $group y accumulators.
+//        b. Resolver con expresiones sobre arreglos (por ejemplo, $sum) pero sin $group.
+//        c. Resolver como en el punto b) pero usar $reduce para calcular la puntuación total.
+//        d. Resolver con find.
 // ---------------------------------------------------------------------------------------
+
+// a)
+db.restaurants.aggregate([
+  {
+    $unwind: "$grades",
+  },
+  {
+    $group: {
+      _id: { restaurant_id: "$restaurant_id", name: "$name" },
+      grade_sum: {
+        $sum: "$grades.score",
+      },
+      grade_max: {
+        $max: "$grades.score",
+      },
+      grade_min: {
+        $min: "$grades.score",
+      },
+    },
+  },
+  {
+    $project: {
+      restaurant_id: "$_id.restaurant_id",
+      name: "$_id.name",
+      grade_sum: 1,
+      grade_max: 1,
+      grade_min: 1,
+      _id: 0,
+    },
+  },
+]);
+
+// b)
+db.restaurants.aggregate([
+  {
+    $match: {
+      "grades.score": { $exists: true, $type: "number" },
+    },
+  },
+  {
+    $project: {
+      restaurant_id: 1,
+      name: 1,
+      grade_sum: {
+        $sum: "$grades.score",
+      },
+      grade_max: {
+        $max: "$grades.score",
+      },
+      grade_min: {
+        $min: "$grades.score",
+      },
+      _id: 0,
+    },
+  },
+]);
+
+// c)
+db.restaurants.aggregate([
+  {
+    // NOTE: Usando reduce, no hace falta matchear si el score es un numero
+    //       porque si esta el campo o no es un numero, reduce no lo va a sumar.
+    //       dando como resultado el de initialValue.
+    $match: {
+      "grades.score": { $exists: true, $type: "number" },
+    },
+  },
+  {
+    $project: {
+      restaurant_id: 1,
+      name: 1,
+      grade_stats: {
+        $reduce: {
+          input: "$grades",
+          initialValue: {
+            sum: 0,
+            max: -Infinity,
+            min: Infinity,
+          },
+          in: {
+            sum: { $add: ["$$value.sum", "$$this.score"] },
+            max: { $max: ["$$value.max", "$$this.score"] },
+            min: { $min: ["$$value.min", "$$this.score"] },
+          },
+        },
+      },
+      _id: 0,
+    },
+  },
+]);
+
+// d)
+db.restaurants.find(
+  // NOTE: Usando reduce, no hace falta matchear si el score es un numero
+  //       porque si esta el campo o no es un numero, reduce no lo va a sumar.
+  //       dando como resultado el de initialValue.
+  {},
+  {
+    restaurant_id: 1,
+    name: 1,
+    grade_stats: {
+      $reduce: {
+        input: "$grades",
+        initialValue: {
+          sum: 0,
+          max: -Infinity,
+          min: Infinity,
+        },
+        in: {
+          sum: { $add: ["$$value.sum", "$$this.score"] },
+          max: { $max: ["$$value.max", "$$this.score"] },
+          min: { $min: ["$$value.min", "$$this.score"] },
+        },
+      },
+    },
+    _id: 0,
+  }
+);
 
 // ---------------------------------------------------------------------------------------
 // 13. Actualizar los datos de los restaurantes añadiendo dos campos nuevos.
-// Se debe actualizar con una sola query.
-//      a. "average_score": con la puntuación promedio
-//      b. "grade": con "A" si "average_score" está entre 0 y 13, 
-//                  con "B" si "average_score" está entre 14 y 27 
-//                  con "C" si "average_score" es mayor o igual a 28    
-// HINT1. Se puede usar pipeline de agregación con la operación update
-// HINT2. El operador $switch o $cond pueden ser de ayuda.
+//     Se debe actualizar con una sola query.
+//        a. "average_score": con la puntuación promedio
+//        b. "grade": con "A" si "average_score" está entre 0 y 13,
+//                    con "B" si "average_score" está entre 14 y 27
+//                    con "C" si "average_score" es mayor o igual a 28
+//     HINT1. Se puede usar pipeline de agregación con la operación update
+//     HINT2. El operador $switch o $cond pueden ser de ayuda.
 // ---------------------------------------------------------------------------------------
