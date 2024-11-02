f62145d4e2a78497f679524d977c459036ffedca
ramiro-l <ramideporte@gmail.com>
2024-11-02 10:49:23 -0300

    feat: se abanzo bastante con el p3 de mongo.

--- MONGODB/Practico3/CONSULTAS.mongodb.js
+++ MONGODB/Practico3/CONSULTAS.mongodb.js
@@ -5,8 +5,8 @@
 use("mflix");
 
 // ---------------------------------------------------------------------------------------
-// Agregar las siguientes reglas de validación usando JSON Schema. Luego de cada 
-// especificación testear que efectivamente las reglas de validación funcionen, 
+// Agregar las siguientes reglas de validación usando JSON Schema. Luego de cada
+// especificación testear que efectivamente las reglas de validación funcionen,
 // intentando insertar 5 documentos válidos y 5 inválidos (por distintos motivos).
 // ---------------------------------------------------------------------------------------
 
@@ -27,20 +27,23 @@ db.runCommand({
         name: {
           bsonType: "string",
           maxLength: 30,
-          description: "debe ser un string de maximo 30 caracteres y es requerido"
+          description:
+            "debe ser un string de maximo 30 caracteres y es requerido",
         },
         email: {
           bsonType: "string",
           pattern: "^(.*)@(.*)\\.(.{2,4})$",
-          description: "debe ser un string tal tiene un nombre de usuario seguido de @, un dominio con una extensión de 2 a 4 caracteres y es requerido"
+          description:
+            "debe ser un string tal tiene un nombre de usuario seguido de @, un dominio con una extensión de 2 a 4 caracteres y es requerido",
         },
         password: {
           bsonType: "string",
           minLength: 50,
-          description: "debe ser un string de minimo 50 caracteres y es requerido"
-        }
-      }
-    }
+          description:
+            "debe ser un string de minimo 50 caracteres y es requerido",
+        },
+      },
+    },
   },
 });
 
@@ -49,57 +52,57 @@ db.runCommand({
 // Casos validos
 db.users.insertMany([
   {
-    "name": "012345678901234567890123456789",
-    "email": "1@test.com",
-    "password": "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu"
+    name: "012345678901234567890123456789",
+    email: "1@test.com",
+    password: "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu",
   },
   {
-    "name": "012345678901234567890123456789",
-    "email": "2@test.com",
-    "password": "01234567890123456789012345678901234567890123456789"
+    name: "012345678901234567890123456789",
+    email: "2@test.com",
+    password: "01234567890123456789012345678901234567890123456789",
   },
   {
-    "name": "3",
-    "email": "3@test.com",
-    "password": "01234567890123456789012345678901234567890123456789"
+    name: "3",
+    email: "3@test.com",
+    password: "01234567890123456789012345678901234567890123456789",
   },
   {
-    "name": "4",
-    "email": "4@test.com",
-    "password": "012345678901234567890123456789012345678901234567890"
+    name: "4",
+    email: "4@test.com",
+    password: "012345678901234567890123456789012345678901234567890",
   },
   {
-    "name": "5",
-    "email": "5@test.com",
-    "password": "012345678901234567890123456789012345678901234567890"
-  }
+    name: "5",
+    email: "5@test.com",
+    password: "012345678901234567890123456789012345678901234567890",
+  },
 ]);
 
 // Casos invalidos
 db.users.insertOne({
-  "name": "012345678901234567890123456789***",
-  "email": "1@test.com",
-  "password": "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu"
+  name: "012345678901234567890123456789***",
+  email: "1@test.com",
+  password: "$2b$12$UREFwsRUoyF0CRqGNK0LzO0HM/jLhgUCNNIJ9RJAqMUQ74crlJ1Vu",
 });
 db.users.insertOne({
-  "name": "012345678901234567890123456789",
-  "email": "2test.com",
-  "password": "01234567890123456789012345678901234567890123456789"
+  name: "012345678901234567890123456789",
+  email: "2test.com",
+  password: "01234567890123456789012345678901234567890123456789",
 });
 db.users.insertOne({
-  "name": "3",
-  "email": "3@test.12345",
-  "password": "01234567890123456789012345678901234567890123456789"
+  name: "3",
+  email: "3@test.12345",
+  password: "01234567890123456789012345678901234567890123456789",
 });
 db.users.insertOne({
-  "name": "4",
-  "email": "4@test.com",
-  "password": "012345678901234567890123456789012345678901234567890"
+  name: "4",
+  email: "4@test.com",
+  password: "012345678901234567890123456789012345678901234567890",
 });
 db.users.insertOne({
-  "name": "5",
-  "email": "5@test.com",
-  "password": "0"
+  name: "5",
+  email: "5@test.com",
+  password: "0",
 });
 
 // ---------------------------------------------------------------------------------------
@@ -107,7 +110,7 @@ db.users.insertOne({
 //    fueron correctamente aplicadas.
 // ---------------------------------------------------------------------------------------
 
-db.getCollectionInfos({ "name": "users" });
+db.getCollectionInfos({ name: "users" });
 
 // ---------------------------------------------------------------------------------------
 // 3. Especificar en la colección theaters las siguientes reglas de validación: El campo
@@ -116,7 +119,7 @@ db.getCollectionInfos({ "name": "users" });
 //         y zipcode todos de tipo string y requeridos
 //      b. un campo geo(no requerido) que sea un object con un campo type, con valores
 //         posibles “Point” o null y coordinates que debe ser una lista de 2 doubles
-//      Por último, estas reglas de validación no deben prohibir la inserción o 
+//      Por último, estas reglas de validación no deben prohibir la inserción o
 //      actualización de documentos que no las cumplan sino que solamente deben advertir.
 // ---------------------------------------------------------------------------------------
 
@@ -129,7 +132,7 @@ db.runCommand({
       properties: {
         theaterId: {
           bsonType: "int",
-          description: "debe ser un int y es requerido"
+          description: "debe ser un int y es requerido",
         },
         location: {
           bsonType: "object",
@@ -139,11 +142,23 @@ db.runCommand({
               bsonType: "object",
               required: ["street1", "city", "state", "zipcode"],
               properties: {
-                street1: { bsonType: "string", description: "debe ser un string y es requerido" },
-                city: { bsonType: "string", description: "debe ser un string y es requerido" },
-                state: { bsonType: "string", description: "debe ser un string y es requerido" },
-                zipcode: { bsonType: "string", description: "debe ser un string y es requerido" },
-              }
+                street1: {
+                  bsonType: "string",
+                  description: "debe ser un string y es requerido",
+                },
+                city: {
+                  bsonType: "string",
+                  description: "debe ser un string y es requerido",
+                },
+                state: {
+                  bsonType: "string",
+                  description: "debe ser un string y es requerido",
+                },
+                zipcode: {
+                  bsonType: "string",
+                  description: "debe ser un string y es requerido",
+                },
+              },
             },
             geo: {
               bsonType: "object",
@@ -159,16 +174,17 @@ db.runCommand({
                   items: {
                     bsonType: "double",
                   },
-                  description: "debe ser una lista que contenga exactamente 2 doubles y no es requerido"
-                }
-              }
+                  description:
+                    "debe ser una lista que contenga exactamente 2 doubles y no es requerido",
+                },
+              },
             },
-          }
-        }
+          },
+        },
       },
-    }
+    },
   },
-  validationAction: "error"
+  validationAction: "warn", // NOTE: cambiar por "error" para debuggear
 });
 
 // db.theaters.findOne();
@@ -177,77 +193,66 @@ db.runCommand({
 
 // Casos validos
 db.theaters.insertOne({
-  "theaterId": 99999,
-  "location": {
-    "address": {
-      "street1": "340asd W Market",
-      "city": "Bloasdomington",
-      "state": "MNasd",
-      "zipcode": "55sad425"
+  theaterId: 99999,
+  location: {
+    address: {
+      street1: "340asd W Market",
+      city: "Bloasdomington",
+      state: "MNasd",
+      zipcode: "55sad425",
     },
-    "geo": {
-      "type": "Point",
-      "coordinates": [
-        -83.24565,
-        42.85466
-      ]
-    }
-  }
+    geo: {
+      type: "Point",
+      coordinates: [-83.24565, 42.85466],
+    },
+  },
 });
 
 // Casos no validos
 db.theaters.insertOne({
-  "theaterId": "99999",
-  "location": {
-    "address": {
-      "street1": "340asd W Market",
-      "city": "Bloasdomington",
-      "state": "MNasd",
-      "zipcode": "55sad425"
+  theaterId: "99999",
+  location: {
+    address: {
+      street1: "340asd W Market",
+      city: "Bloasdomington",
+      state: "MNasd",
+      zipcode: "55sad425",
     },
-    "geo": {
-      "type": "Point",
-      "coordinates": [
-        -83.24565,
-        42.85466
-      ]
-    }
-  }
+    geo: {
+      type: "Point",
+      coordinates: [-83.24565, 42.85466],
+    },
+  },
 });
 db.theaters.insertOne({
-  "theaterId": 99999,
-  "location": {
-    "address": {
-      "street1": 1,
-      "city": "Bloasdomington",
-      "state": "MNasd",
-      "zipcode": "55sad425"
+  theaterId: 99999,
+  location: {
+    address: {
+      street1: 1,
+      city: "Bloasdomington",
+      state: "MNasd",
+      zipcode: "55sad425",
     },
-    "geo": {
-      "type": "Point",
-      "coordinates": [
-        -83.24565,
-        42.85466
-      ]
-    }
-  }
+    geo: {
+      type: "Point",
+      coordinates: [-83.24565, 42.85466],
+    },
+  },
 });
 db.theaters.insertOne({
-  "theaterId": 99999,
-  "location": {
-    "address": {
-      "street1": 1,
-      "city": "Bloasdomington",
-      "state": "MNasd",
-      "zipcode": "55sad425"
+  theaterId: 99999,
+  location: {
+    address: {
+      street1: 1,
+      city: "Bloasdomington",
+      state: "MNasd",
+      zipcode: "55sad425",
     },
-    "geo": {
-      "type": "Point",
-      "coordinates": [
-        -83.24565
-      ]
-    }
-  }
+    geo: {
+      type: "Point",
+      coordinates: [-83.24565],
+    },
+  },
 });
 
 // ---------------------------------------------------------------------------------------
@@ -260,29 +265,299 @@ db.theaters.insertOne({
 //          en javascript los literales numéricos son de tipo Number(double).
 // ---------------------------------------------------------------------------------------
 
+// db.movies.findOne();
+
+db.runCommand({
+  collMod: "movies",
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["title", "year"],
+      properties: {
+        title: {
+          bsonType: "string",
+          description: "debe ser un string y es requerido",
+        },
+        year: {
+          bsonType: "int",
+          minimum: 1900,
+          maximum: 3000,
+          description: "debe ser un int entre 1900 y 3000 y es requerido",
+        },
+        cast: {
+          bsonType: "array",
+          uniqueItems: true,
+          items: {
+            bsonType: "string",
+          },
+          description: "debe ser un array de strings sin duplicados",
+        },
+        directors: {
+          bsonType: "array",
+          uniqueItems: true,
+          items: {
+            bsonType: "string",
+          },
+          description: "debe ser un array de strings sin duplicados",
+        },
+        countries: {
+          bsonType: "array",
+          uniqueItems: true,
+          items: {
+            bsonType: "string",
+          },
+          description: "debe ser un array de strings sin duplicados",
+        },
+        genres: {
+          bsonType: "array",
+          uniqueItems: true,
+          items: {
+            bsonType: "string",
+          },
+          description: "debe ser un array de strings sin duplicados",
+        },
+      },
+    },
+  },
+});
+
+// Casos validos
+db.movies.insertOne({
+  title: "The title",
+  year: NumberInt(2021),
+  cast: ["actor1", "actor2"],
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: ["genre1", "genre2"],
+});
+
+// Casos no validos
+db.movies.insertOne({
+  // title: "The title",
+  year: NumberInt(2021),
+  cast: ["actor1", "actor2"],
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: ["genre1", "genre2"],
+});
+
+db.movies.insertOne({
+  title: "The title",
+  year: "2021", // Debe ser un int
+  cast: ["actor1", "actor2"],
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: ["genre1", "genre2"],
+});
+
+db.movies.insertOne({
+  title: "The title",
+  year: NumberInt(2021),
+  cast: ["actor1", 1], // debe ser un array de strings
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: ["genre1", "genre2"],
+});
+
+db.movies.insertOne({
+  title: "The title",
+  year: NumberInt(2021),
+  cast: ["actor1", "actor2"],
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: 4, // debe ser un array
+});
+
+db.movies.insertOne({
+  title: "The title",
+  year: NumberInt(3001), // La fecha debe ser menor o igual a 3000
+  cast: ["actor1", "actor2"],
+  directors: ["director1", "director2"],
+  countries: ["country1", "country2"],
+  genres: ["genre1", "genre2"],
+});
+
 // ---------------------------------------------------------------------------------------
 // 5. Crear una colección userProfiles con las siguientes reglas de validación: Tenga un
-//    campo user_id(requerido) de tipo “objectId”, un campo language(requerido) con alguno
-//    de los siguientes valores[ “English”, “Spanish”, “Portuguese” ] y un campo
-//    favorite_genres(no requerido) que sea un array de strings sin duplicados.
+//    campo user_id (requerido) de tipo “objectId”, un campo language (requerido) con alguno
+//    de los siguientes valores [ “English”, “Spanish”, “Portuguese” ] y un campo
+//    favorite_genres (no requerido) que sea un array de strings sin duplicados.
 // ---------------------------------------------------------------------------------------
+
+db.createCollection("userProfiles", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["user_id", "language"],
+      properties: {
+        user_id: {
+          bsonType: "objectId",
+          description: "debe ser un “objectId” y es requerido",
+        },
+        language: {
+          bsonType: "string",
+          enum: ["English", "Spanish", "Portuguese"],
+          description:
+            "debe ser 'English', 'Spanish' o 'Portuguese' y es requirido",
+        },
+        favorite_genres: {
+          bsonType: "array",
+          uniqueItems: true,
+          items: {
+            bsonType: "string",
+          },
+          description: "debe ser un array de strings sin duplicados",
+        },
+      },
+    },
+  },
+});
+
+// Casos validos
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "English",
+  favorite_genres: ["genre1", "genre2"],
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "Spanish",
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "Portuguese",
+  favorite_genres: ["genre1"],
+});
+
+// Casos no validos
+db.userProfiles.insertOne({
+  // user_id: ObjectId(),
+  language: "English",
+  favorite_genres: ["genre1", "genre2"],
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  // language: "English",
+  favorite_genres: ["genre1", "genre2"],
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "English",
+  favorite_genres: [1, "genre2"], // Debe ser un array de strings
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "Cordobes", // Debe ser 'English', 'Spanish' o 'Portuguese'
+  favorite_genres: ["genre1", "genre2"],
+});
+
+db.userProfiles.insertOne({
+  user_id: ObjectId(),
+  language: "English",
+  favorite_genres: 1, // Debe ser un array
+});
 
 // ---------------------------------------------------------------------------------------
 // Modelado de datos en MongoDB
 // ---------------------------------------------------------------------------------------
-// 6. Identificar los distintos tipos de relaciones(One - To - One, One - To - Many) en las
-//    colecciones movies y comments.Determinar si se usó documentos anidados o
+// 6. Identificar los distintos tipos de relaciones(One-To-One, One-To-Many) en las
+//    colecciones movies y comments. Determinar si se usó documentos anidados o
 //    referencias en cada relación y justificar la razón.
 // ---------------------------------------------------------------------------------------
+
+// db.movies.findOne();
+// db.comments.findOne();
+
+// Respuesta: Se uso la relacion one-to-many (uno a muchos) de movies a comments,
+//            utilizando referencias. La razon es porque una pelicula puede tener
+//            muchos comentarios, no tiene sentido usar anidados porque no escala.
+//            Y es de tipo 1 a muchos porque justamente una peli tiene muchos
+//            comentarios y no uno solo.
 
 // ---------------------------------------------------------------------------------------
 // 7. (leer de la consigna, tiene imagenes)
 // ---------------------------------------------------------------------------------------
+// Queries comunes:
+//  I.   Listar el id, titulo, y precio de los libros y sus categorías de un autor en particular
+//  II.  Cantidad de libros por categorías
+//  III. Listar el nombre y dirección entrega y el monto total (quantity * price) de sus
+//       pedidos para un order_id dado
+
+db.createCollection("books", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        book_id: {
+          bsonType: "objectId",
+        },
+        title: {
+          bsonType: "string",
+        },
+        author: {
+          bsonType: "string",
+        },
+        price: {
+          bsonType: "double",
+        },
+        category: {
+          bsonType: "string",
+        },
+      },
+    },
+  },
+});
+
+db.createCollection("orders", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      properties: {
+        delivery_name: {
+          bsonType: "string",
+        },
+        delivery_address: {
+          bsonType: "string",
+        },
+        cc_name: {
+          bsonType: "string",
+        },
+        cc_number: {
+          bsonType: "string",
+        },
+        cc_expiry: {
+          bsonType: "string",
+        },
+        books_details: {
+          bsonType: "array",
+          items: {
+            bsonType: "object",
+            properties: {
+              title: {
+                bsonType: "string",
+              },
+              author: {
+                bsonType: "string",
+              },
+              price: {
+                bsonType: "double",
+              },
+              quantuty: {
+                bsonType: "int",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+});
 
 // ---------------------------------------------------------------------------------------
 // 8. (leer de la consigna, tiene imagenes)
-// ---------------------------------------------------------------------------------------
-
-// ---------------------------------------------------------------------------------------
-// 9. (leer de la consigna, tiene imagenes)
 // ---------------------------------------------------------------------------------------
