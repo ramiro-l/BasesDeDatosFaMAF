229f50795e8536852bcd45c74fc1022fea6316c9
Ramiro Lugo <74385260+ramiro-l@users.noreply.github.com>
2024-11-07 20:27:15 +0000

    feat: se empezo a practicar para el parcial 2 de mongo.

---
+++ MONGODB/Practica-Parcial2/2023/CONSULTAS.mongodb.js
@@ -0,0 +1,136 @@
+
+// -----------------------------------------------------------------------------------------
+// 1.Buscar las ventas realizadas en "London", "Austin" o "San Diego"; a un customer con
+// edad mayor-igual a 18 años que tengan productos que hayan salido al menos 1000
+// y estén etiquetados (tags) como de tipo "school" o "kids" (pueden tener más
+// etiquetas).
+// Mostrar el id de la venta con el nombre "sale", la fecha (“saleDate"), el storeLocation,
+// y el "email del cliente. No mostrar resultados anidados.
+// -----------------------------------------------------------------------------------------
+
+db.sales.find({
+    "storeLocation": { $in: ["London", "Austin", "San Diego"] },
+    "customer.age": {
+        $gte: 18
+    },
+    "items": {
+        $elemMatch: {
+            "price": { $gte: 1000 },
+            "tags": { $in: ["school", "kids"] }
+        },
+    }
+}, {
+    "sale": "$_id",
+    "saleDate": 1,
+    "storeLocation": 1,
+    "email": "$customer.email",
+    "_id": 0
+});
+
+// -----------------------------------------------------------------------------------------
+// 2. Buscar las ventas de las tiendas localizadas en Seattle, donde el método de compra
+// sea ‘In store’ o ‘Phone’ y se hayan realizado entre 1 de febrero de 2014 y 31 de enero
+// de 2015 (ambas fechas inclusive). Listar el email y la satisfacción del cliente, y el
+// monto total facturado, donde el monto de cada item se calcula como 'price *
+// quantity'. Mostrar el resultado ordenados por satisfacción (descendente), frente a
+// empate de satisfacción ordenar por email (alfabético).
+// -----------------------------------------------------------------------------------------
+
+// db.sales.findOne();
+
+db.sales.aggregate([
+    {
+        $match: {
+            "storeLocation": "Seattle",
+            "purchaseMethod": { $in: ["In store", "Phone"] },
+            "saleDate": {
+                $gte: ISODate("2014-02-01T00:00:00Z"),
+                $lte: ISODate("2015-01-31T23:59:59Z"),
+            },
+        }
+    },
+    {
+        $project: {
+            "emailCustomer": "$customer.email",
+            "satisfactionCustomer": "$customer.satisfaction",
+            totalFacturado: {
+                $sum: {
+                    $map: {
+                        input: "$items",
+                        as: "item",
+                        in: { $multiply: ["$$item.price", "$$item.quantity"] }
+                    }
+                }
+            }
+        },
+    },
+    {
+        $sort: {
+            "satisfactionCustomer": 1,
+            "emailCustomer": 1
+        }
+    }
+]);
+
+// -----------------------------------------------------------------------------------------
+// 3. Crear la vista salesInvoiced que calcula el monto mínimo, monto máximo, monto
+// total y monto promedio facturado por año y mes. Mostrar el resultado en orden
+// cronológico. No se debe mostrar campos anidados en el resultado.
+// -----------------------------------------------------------------------------------------
+
+// db.sales.findOne();
+
+db.createView("salesInvoiced", "sales", [
+    {
+        $project: {
+            "saleDate": 1,
+            "totalFacturado": {
+                $sum: {
+                    $map: {
+                        input: "$items",
+                        as: "item",
+                        in: { $multiply: ["$$item.price", "$$item.quantity"] }
+                    }
+                }
+            }
+        },
+    },
+    {
+        $group: {
+            _id: {
+                year: { $year: "$saleDate" },
+                month: { $month: "$saleDate" }
+            },
+            min_facturado: { $min: "$totalFacturado" },
+            max_facturado: { $max: "$totalFacturado" },
+            avg_facturado: { $avg: "$totalFacturado" },
+            sum_facturado: { $sum: "$totalFacturado" }
+        },
+    },
+    {
+        $sort: {
+            "_id.year": 1,
+            "_id.month": 1
+        }
+    }
+]);
+
+// db.salesInvoiced.find();
+// db.salesInvoiced.drop();
+
+// -----------------------------------------------------------------------------------------
+// 4. Mostrar el storeLocation, la venta promedio de ese local, el objetivo a cumplir de
+// ventas (dentro de la colección storeObjectives) y la diferencia entre el promedio y el
+// objetivo de todos los locales.
+// -----------------------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------------------
+// 5. Especificar reglas de validación en la colección sales utilizando JSON Schema.
+//      a. Las reglas se deben aplicar sobre los campos: saleDate, storeLocation,
+//         purchaseMethod, y customer ( y todos sus campos anidados ). Inferir los
+//         tipos y otras restricciones que considere adecuados para especificar las
+//         reglas a partir de los documentos de la colección.
+//      b. Para testear las reglas de validación crear un caso de falla en la regla de
+//         validación y un caso de éxito (Indicar si es caso de falla o éxito)
+// -----------------------------------------------------------------------------------------
